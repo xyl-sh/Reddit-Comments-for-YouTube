d0a6d8449efafc97c7ff1e8df31dcf7221e52f32
Xyl <git@xyl.sh>
2025-04-14 10:41:57 +1000

    Refactor :global wrapping

--- src/components/threads/CommentComponent.svelte
+++ src/components/threads/CommentComponent.svelte
@@ -132,63 +132,65 @@
 	.reddit-comment {
 		@apply text-[14px] text-primary;
 
-		:global(*) {
-			@apply my-[5px] break-words;
-		}
-
-		:global(p > img) {
-			@apply my-0;
-		}
-
-		:global(a),
-		:global(button) {
-			@apply plain-button;
-
-			&:not([onclick*="spoiler"]) {
-				@apply text-link;
+		:global {
+			* {
+				@apply my-[5px] break-words;
 			}
-		}
 
-		:global(.reddit-spoiler) {
-			:global(&:not(.show-spoiler)) {
-				:global(> button > span) {
-					@apply bg-[var(--subtle-link-text)] text-transparent;
+			p > img {
+				@apply my-0;
+			}
+
+			a,
+			button {
+				@apply plain-button;
+
+				&:not([onclick*="spoiler"]) {
+					@apply text-link;
+				}
+			}
+
+			.reddit-spoiler {
+				&:not(.show-spoiler) {
+					> button > span {
+						@apply bg-[var(--subtle-link-text)] text-transparent;
+					}
+
+					> span {
+						@apply hidden;
+					}
 				}
 
-				:global(> span) {
+				&.show-spoiler > button {
 					@apply hidden;
 				}
 			}
 
-			:global(&.show-spoiler > button) {
-				@apply hidden;
+			blockquote {
+				@apply text-secondary px-2 ml-[5px] border-0 border-l-2 border-l-separator border-solid;
 			}
-		}
 
-		:global(blockquote) {
-			@apply text-secondary px-2 ml-[5px] border-0 border-l-2 border-l-separator border-solid;
-		}
+			pre {
+				@apply bg-interactable border-interactable rounded-sm py-1 px-[9px];
+			}
 
-		:global(pre) {
-			@apply bg-interactable border-interactable rounded-sm py-1 px-[9px];
-		}
+			ol,
+			ul {
+				@apply pl-10;
+			}
 
-		:global(ol),
-		:global(ul) {
-			@apply pl-10;
-		}
+			table {
+				@apply border-collapse;
+			}
 
-		:global(table) {
-			@apply border-collapse;
-		}
+			th,
+			td {
+				@apply p-[5px] border-[1px] border-blockquote;
+			}
 
-		:global(th),
-		:global(td) {
-			@apply p-[5px] border-[1px] border-blockquote;
-		}
-
-		:global(code) {
-			@apply mx-0.5 break-normal leading-5 w-full inline-block overflow-x-auto;
+			code {
+				@apply mx-0.5 break-normal leading-5 w-full inline-block overflow-x-auto;
+			}
 		}
 	}
 </style>
